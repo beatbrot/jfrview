3c7f13b3487ac8c8134f17e2a510a1dcf07fdff0
Christoph Loy <loy.christoph@gmail.com>
2024-07-27 17:26:37 +0200

    Add web target

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{env, error::Error, fs::File};
-
+use eframe::WebRunner;
 use flame_graph::FlameGraph;
 
 use crate::ui::app::JfrViewApp;
@@ -8,12 +8,26 @@ mod data;
 mod flame_graph;
 mod ui;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), Box<dyn Error>> {
     env::set_var("RUST_BACKTRACE", "1");
     let fg = create_flame_graph("C:\\Users\\loych\\Development\\jfrview\\cfg6_validate_small.jfr");
 
     eframe::run_native("JfrView", Default::default(), Box::new(|_| Ok(Box::new(JfrViewApp::new(fg)))))?;
     Ok(())
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    let fg = FlameGraph::default();
+
+    let web_options = eframe::WebOptions::default();
+    wasm_bindgen_futures::spawn_local(async {
+       let start_result = WebRunner::new()
+           .start("canvas", web_options, Box::new(|_| Ok(Box::new(JfrViewApp::new(fg)))))
+           .await.unwrap();
+    });
 }
 
 fn create_flame_graph(path: &str) -> FlameGraph {
