2a34f402b0d31aac171e4137bdeb18b9e785e07d
Christoph Loy <loy.christoph@gmail.com>
2024-12-30 14:54:50 +0100

    Remove intermediate representation of flame graphs

--- src/export.rs
+++ src/export.rs
@@ -1,35 +1,136 @@
-use crate::flame_graph::{FlameGraph, Frame};
+use std::{
+    hash::Hash,
+    io::{Read, Seek},
+};
+
+use crate::data::ExecutionSample;
+use indexmap::{set::MutableValues, IndexSet};
 use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Sample {
     name: String,
     value: usize,
-    children: Vec<Sample>,
+    children: IndexSet<Sample>,
+}
+
+impl PartialEq for Sample {
+    fn eq(&self, other: &Self) -> bool {
+        return self.name.eq(&other.name);
+    }
+}
+
+impl Eq for Sample {}
+
+impl Hash for Sample {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
 }
 
 impl Sample {
-    pub fn from(fg: FlameGraph, include_native: bool) -> Self {
-        fn handle(frame: &Frame, include_native: bool) -> Sample {
-            Sample {
-                name: format!("{:?}", frame.method),
-                value: frame.ticks(include_native),
-                children: frame
-                    .children
-                    .values()
-                    .map(|m| handle(m, include_native))
-                    .collect(),
-            }
-        }
+    pub fn from_file<T>(input: T, include_native: bool) -> anyhow::Result<Self>
+    where
+        T: Read + Seek,
+    {
+        let mut result = Self::new("root".to_string());
+        ExecutionSample::visit_events(input, |e| result.add_sample(&e, include_native))?;
 
-        Self {
-            name: "Root".to_string(),
-            value: fg.ticks(include_native),
-            children: fg
-                .frames
-                .values()
-                .map(|m| handle(m, include_native))
-                .collect(),
+        Ok(result)
+    }
+
+    fn add_sample(&mut self, sample: &ExecutionSample, include_native: bool) {
+        if sample.native && !include_native {
+            return;
         }
+        let mut curr = self;
+        curr.value += 1;
+        for frame in sample.stack_trace.frames.iter() {
+            let name = format!("{:?}", frame.method);
+            let (idx, _) = curr.children.insert_full(Self::new(name));
+            let sample = curr.children.get_index_mut2(idx).unwrap();
+            sample.value += 1;
+            curr = sample;
+        }
+    }
+
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            value: 0,
+            children: Default::default(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl core::fmt::Display for Sample {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn printer(
+            f: &mut std::fmt::Formatter<'_>,
+            indent: usize,
+            sample: &Sample,
+        ) -> std::fmt::Result {
+            writeln!(
+                f,
+                "{}{}: ({})",
+                "| ".repeat(indent),
+                sample.name,
+                sample.value
+            )?;
+            for c in &sample.children {
+                printer(f, indent + 1, c)?;
+            }
+            Ok(())
+        }
+        for c in &self.children {
+            printer(f, 0, c)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use insta::{assert_snapshot, glob};
+    use std::fs::File;
+
+    use crate::export::Sample;
+
+    #[test]
+    fn parse_without_panic_nonnative() {
+        glob!("../test-data", "*.jfr", |path| {
+            let f = File::open(path).unwrap();
+            let flame_graph = Sample::from_file(f, false);
+            let file_name = path.file_name().unwrap().to_string_lossy();
+            insta::with_settings!({
+                omit_expression => true,
+                description => format!("non-native, {}", file_name)
+            }, {
+                assert_snapshot!(flame_graph.unwrap());
+            });
+        });
+    }
+
+    #[test]
+    fn parse_without_panic_native() {
+        glob!("../test-data", "*.jfr", |path| {
+            let f = File::open(path).unwrap();
+            let flame_graph = Sample::from_file(f, true);
+            let file_name = path.file_name().unwrap().to_string_lossy();
+            insta::with_settings!({
+                omit_expression => true,
+                description => format!("native, {}", file_name)
+            }, {
+                assert_snapshot!(flame_graph.unwrap());
+            });
+        });
+    }
+
+    #[test]
+    fn test_invalid() -> anyhow::Result<()> {
+        let file = File::open("test-data/invalid.jfr.fail")?;
+        assert!(Sample::from_file(file, false).is_err());
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 use crate::export::Sample;
-use crate::flame_graph::FlameGraph;
+use std::fmt::Debug;
 use std::io::Cursor;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
 
 mod data;
 mod export;
-mod flame_graph;
 
 #[wasm_bindgen]
 pub fn parse(input: Vec<u8>, include_native: bool) -> Result<JsValue, String> {
-    let fg = FlameGraph::try_new(Cursor::new(input)).map_err(|e| format!("{e:?}"))?;
-    let export = Sample::from(fg, include_native);
-    serde_wasm_bindgen::to_value(&export).map_err(|e| format!("{e:?}"))
+    let cursor = Cursor::new(input);
+    let export = Sample::from_file(cursor, include_native).map_err(to_str)?;
+    serde_wasm_bindgen::to_value(&export).map_err(to_str)
+}
+
+fn to_str(t: impl Debug) -> String {
+    format!("{t:?}")
 }
