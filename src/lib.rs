6202655f58282c95ac4a949edc08e728f1d3b17e
Christoph Loy <loy.christoph@gmail.com>
2025-11-27 20:20:37 +0100

    More cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
-use crate::export::Sample;
 use std::fmt::Debug;
 use std::io::Cursor;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 mod data;
-mod export;
 mod speedscope;
-
-#[wasm_bindgen]
-pub fn parse(input: Vec<u8>, include_native: bool, threads: bool) -> Result<JsValue, String> {
-    let cursor = Cursor::new(input);
-    let export = Sample::from_file(cursor, include_native, threads).map_err(to_str)?;
-    serde_wasm_bindgen::to_value(&export).map_err(to_str)
-}
 
 #[wasm_bindgen]
 pub fn interpret_jfr(input: Vec<u8>, include_native: bool) -> Result<JsValue, String> {
