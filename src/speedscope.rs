fa311da591dc980f185251af9f87c95c60eff0de
Christoph Loy <loy.christoph@gmail.com>
2025-12-11 17:43:16 +0100

    Avoid intermediate data model

--- src/speedscope.rs
+++ src/speedscope.rs
@@ -1,11 +1,87 @@
 use std::io::{Read, Seek};
 
+use anyhow::{Context, Ok, Result};
+use jfrs::reader::{JfrReader, event::Accessor, value_descriptor::ValueDescriptor};
 use serde::Serialize;
 use string_cache::DefaultAtom;
 
-use crate::data::{ExecutionSample, StackFrame};
+use crate::data::{EXEC_SAMPLE, ExecutionSample, NATIVE_EXEC_SAMPLE, StackFrame};
 
 pub fn export<T>(input: T) -> anyhow::Result<Vec<MethodSample>>
+where
+    T: Read + Seek,
+{
+    let mut result = Vec::<MethodSample>::new();
+    let mut reader = JfrReader::new(input);
+
+    for chunk in reader.chunks() {
+        let (mut r, c) = chunk.with_context(|| "Unable to parse chunk.")?;
+
+        for event_res in r.events(&c) {
+            let event = event_res.with_context(|| "Unable to parse event.")?;
+            let cls_name = event.class.name();
+            if cls_name == EXEC_SAMPLE || cls_name == NATIVE_EXEC_SAMPLE {
+                let native = cls_name == NATIVE_EXEC_SAMPLE;
+                let st_raw = event.value().get_field("stackTrace").unwrap();
+                let frames = read_stacktrace(st_raw)?;
+                let sample = MethodSample { frames, native };
+                result.push(sample);
+            }
+        }
+    }
+
+    Ok(result)
+}
+
+fn read_stacktrace(value: Accessor<'_>) -> Result<Vec<Frame>> {
+    let frames_raw = value.get_field("frames").unwrap().as_iter().unwrap();
+    let mut frames = Vec::with_capacity(frames_raw.size_hint().0);
+    for raw_frame in frames_raw {
+        let f = read_frame(raw_frame)?;
+        frames.push(f);
+    }
+    frames.reverse();
+    Ok(frames)
+}
+
+fn read_frame(value: Accessor<'_>) -> Result<Frame> {
+    let raw_method = value.get_field("method").unwrap();
+    let method_name = extract_symbol(&raw_method, "name");
+    let raw_class = raw_method.get_field("type").unwrap();
+    raw_class.get_field("name").unwrap();
+    let cls_name = extract_symbol(&raw_class, "name");
+    let mut result = String::with_capacity(cls_name.len() + 1 + method_name.len());
+    for byte in cls_name.chars() {
+        let mut b = byte;
+        if byte == '/' {
+            b = '.';
+        }
+        result.push(b);
+    }
+    result.push(':');
+    result.push_str(method_name);
+
+    Ok(Frame {
+        name: DefaultAtom::from(result),
+    })
+}
+
+fn extract_symbol<'a>(value: &'a Accessor, name: &str) -> &'a str {
+    let str: &str = extract_primitive(&value.get_field(name).unwrap(), "string");
+    str
+}
+
+fn extract_primitive<'a, T>(value: &Accessor<'a>, name: &str) -> T
+where
+    T: TryFrom<&'a ValueDescriptor> + Default,
+{
+    value
+        .get_field(name)
+        .and_then(|v| <T>::try_from(v.value).ok())
+        .unwrap()
+}
+
+pub fn export_old<T>(input: T) -> anyhow::Result<Vec<MethodSample>>
 where
     T: Read + Seek,
 {
